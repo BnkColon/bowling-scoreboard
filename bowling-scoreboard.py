11782ad0fff796e78c4cef5a6db334eafdf1785a
Bianca Colon <colon.bianca@gmail.com>
2016-11-14 16:19:21 -0400

    Steps before calculate the total score.

--- bowling-scoreboard.py
+++ bowling-scoreboard.py
@@ -3,24 +3,14 @@
 # Filename: bowling-scoreboard.py 
 # To run this code: python bowling-scoreboard 8 1 10 5 5 8 0 10 10 9 1 8 1 9 1 10 7 2
 
-def scoreBoardCalcutator(number):
-	# Frames
+def scoreCalcutator(number):
+	pass
+
+def scoreBoard(number):
 	scoreBoard = {1: None, 2: None, 3: None, 4: None, 5: None, 6: None, 7: None, 8: None, 9: None, 10: None}
-
-	## Strike
-	if number == 10:
-		print 'Strike'
-
-	## Spare
-	#if number + (number+1) == 10:
-	#	print 'Spare'
-
-	## Miss 
-	if number == 0:
-		print 'Miss'
-
+	for k,v in scoreBoard.items():
+		scoreBoard[k] = int(number)
 	return scoreBoard
-
 
 import sys 
 
@@ -34,12 +24,19 @@ for arg in sys.argv[1:]:
 		f.close()
 
 		score = score.split()
+		print score
 		
 		# Send all the scores to the Board Calculator
-		for s in score:
-			scoreBoardCalcutator(s)
+		for s in range(0, len(score)):
+			
+			if score[s] == '10':
+				print 'Strike', score[s]
 
-
+			elif score[s] == '0':
+				print 'Miss', score[s]
+			else:
+				print 'Score', score[s]
+		
 	except IOError:
 		# If can't open the file
 		print 'Cannot open', arg
