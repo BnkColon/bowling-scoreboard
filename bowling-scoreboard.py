d764cc29a9349b61246c3a1d2b8c4845e240bcf0
Bianca Colon <colon.bianca@gmail.com>
2016-11-15 16:00:23 -0400

    Working on score board.

--- bowling-scoreboard.py
+++ bowling-scoreboard.py
@@ -3,11 +3,11 @@
 # Filename: bowling-scoreboard.py 
 # To run this code: python bowling-scoreboard 8 1 10 5 5 8 0 10 10 9 1 8 1 9 1 10 7 2
 
-def scoreCalcutator(number):
+def scoreCalcutator(number, bonus1, bonus2):
 	pass
 
 def scoreBoard(number):
-	scoreBoard = {1: None, 2: None, 3: None, 4: None, 5: None, 6: None, 7: None, 8: None, 9: None, 10: None}
+	scoreBoard = {1: {None, None, None, None}, 2: {None, None, None, None}, 3: {None, None, None, None}, 4: {None, None, None, None}, 5: {None, None, None, None}, 6: {None, None, None, None}, 7: {None, None, None, None}, 8: {None, None, None, None}, 9: {None, None, None, None}, 10: {None, None, None, None}}
 	for k,v in scoreBoard.items():
 		scoreBoard[k] = int(number)
 	return scoreBoard
@@ -30,7 +30,8 @@ for arg in sys.argv[1:]:
 		for s in range(0, len(score)):
 			
 			if score[s] == '10':
-				print 'Strike', score[s]
+				total = scoreCalcutator(score[s], score[s+1], score[s+2])
+				scoreBoard('X', 0, 0, total)
 
 			elif score[s] == '0':
 				print 'Miss', score[s]
