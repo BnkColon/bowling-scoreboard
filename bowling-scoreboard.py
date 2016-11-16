446d9f4fd00d5d8c4a3d200b71e7418582d350fa
Bianca Colon <colon.bianca@gmail.com>
2016-11-16 10:31:33 -0400

    Fixing the functions.

--- bowling-scoreboard.py
+++ bowling-scoreboard.py
@@ -3,14 +3,26 @@
 # Filename: bowling-scoreboard.py 
 # To run this code: python bowling-scoreboard 8 1 10 5 5 8 0 10 10 9 1 8 1 9 1 10 7 2
 
-def scoreCalcutator(number, bonus1, bonus2):
-	pass
+def scoreCalcutator(throw, throw1, throw2):
+	if int(throw) == 10:
+		total = 10 + throw1 + throw2
+	else:
+		if throw + throw1 == 10:
+			total = 10 + throw2
+		else:
+			total = throw + throw1
+	return total 
 
-def scoreBoard(number):
-	scoreBoard = {1: {None, None, None, None}, 2: {None, None, None, None}, 3: {None, None, None, None}, 4: {None, None, None, None}, 5: {None, None, None, None}, 6: {None, None, None, None}, 7: {None, None, None, None}, 8: {None, None, None, None}, 9: {None, None, None, None}, 10: {None, None, None, None}}
-	for k,v in scoreBoard.items():
-		scoreBoard[k] = int(number)
+def scoreBoardTable(throw1, throw2, last, total):
+	scoreBoard = {1: (None, None, None, None), 2: (None, None, None, None), 3: {None, None, None, None}, 4: {None, None, None, None}, 5: {None, None, None, None}, 6: {None, None, None, None}, 7: {None, None, None, None}, 8: {None, None, None, None}, 9: {None, None, None, None}, 10: {None, None, None, None}}
+	total += int(total)
+	for frame,score in scoreBoard.items():
+		if scoreBoard[frame][0] == None:
+			scoreBoard[frame] = {throw1, throw2, last, total}
+		else: 
+			continue
 	return scoreBoard
+
 
 import sys 
 
@@ -27,17 +39,21 @@ for arg in sys.argv[1:]:
 		print score
 		
 		# Send all the scores to the Board Calculator
-		for s in range(0, len(score)):
-			
+		for s in range(0, len(score),2):
 			if score[s] == '10':
-				total = scoreCalcutator(score[s], score[s+1], score[s+2])
-				scoreBoard('X', 0, 0, total)
-
+				total = scoreCalcutator(int(score[s]), int(score[s+1]), int(score[s+2]))
+				final = scoreBoardTable('X', 0, 0, int(total))
 			elif score[s] == '0':
-				print 'Miss', score[s]
+				if score[s+1] != '10':
+					total = scoreCalcutator(int(score[s]), int(score[s+1]), 0)
+					final = scoreBoardTable(int(score[s]), int(score[s+1]), 0, int(total))
+				else:
+					total = scoreCalcutator(int(score[s]), int(score[s+1]), int(score[s+2]))
+					final = scoreBoardTable(int(score[s]), int(score[s+1]), 0, int(total))
 			else:
-				print 'Score', score[s]
-		
+				total = scoreCalcutator(int(score[s]), int(score[s+1]), 0)
+				final = scoreBoardTable(int(score[s]), int(score[s+1]), 0, int(total))
+		print final
 	except IOError:
 		# If can't open the file
 		print 'Cannot open', arg
