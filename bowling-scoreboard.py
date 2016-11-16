9ef3c7a18010418aec009e3290733245ef6666ca
Bianca Colon <colon.bianca@gmail.com>
2016-11-16 18:54:16 -0400

    Add a dictionary to change any possible valid input to its equivalent value.

--- bowling-scoreboard.py
+++ bowling-scoreboard.py
@@ -7,57 +7,44 @@ def scoreBowling(input):
 	pass
 
 def scoreCalcutator(throw, throw1, throw2):
-	if int(throw) == 10:
-		total = 10 + throw1 + throw2
-	else:
-		if throw + throw1 == 10:
-			total = 10 + throw2
-		else:
-			total = throw + throw1
-	return total 
+	pass
 
 def scoreBoardTable(throw1, throw2, last, total):
-	scoreBoard = {1: (None, None, None, None), 2: (None, None, None, None), 3: {None, None, None, None}, 4: {None, None, None, None}, 5: {None, None, None, None}, 6: {None, None, None, None}, 7: {None, None, None, None}, 8: {None, None, None, None}, 9: {None, None, None, None}, 10: {None, None, None, None}}
-	total += int(total)
-	for frame,score in scoreBoard.items():
-		if scoreBoard[frame][0] == None:
-			scoreBoard[frame] = {throw1, throw2, last, total}
-		else: 
-			continue
-	return scoreBoard
-
-
+	pass
 
 import sys 
+
+cases = {
+    'X' : 10,
+    '/' : 10,
+    '-' : 0,
+    '0' : 0,
+    '1' : 1,
+    '2' : 2,
+    '3' : 3,
+    '4' : 4,
+    '5' : 5,
+    '6' : 6,
+    '7' : 7,
+    '8' : 8,
+    '9' : 9,
+    '10': 10}
 
 # Read all the arguments after bowling-scoreboard
 for arg in sys.argv[1:]: 
 	try:
 		# Open the file with scores
 		f = open('./tests/' + str(arg), 'r')
-		score = f.readline()
+		content = f.readline()
 		# Close file 
 		f.close()
 
-		score = score.split()
-		print score
+		inputValues = content.split()
+		print inputValues
 		
 		# Send the input to score Bowling
-		for s in range(0, len(score),2):
-			if score[s] == '10':
-				total = scoreCalcutator(int(score[s]), int(score[s+1]), int(score[s+2]))
-				final = scoreBoardTable('X', 0, 0, int(total))
-			elif score[s] == '0':
-				if score[s+1] != '10':
-					total = scoreCalcutator(int(score[s]), int(score[s+1]), 0)
-					final = scoreBoardTable(int(score[s]), int(score[s+1]), 0, int(total))
-				else:
-					total = scoreCalcutator(int(score[s]), int(score[s+1]), int(score[s+2]))
-					final = scoreBoardTable(int(score[s]), int(score[s+1]), 0, int(total))
-			else:
-				total = scoreCalcutator(int(score[s]), int(score[s+1]), 0)
-				final = scoreBoardTable(int(score[s]), int(score[s+1]), 0, int(total))
-		print final
+		for s in range(0, len(inputValues)):
+			print type(cases[inputValues[s]])
 	except IOError:
 		# If can't open the file
 		print 'Cannot open', arg
